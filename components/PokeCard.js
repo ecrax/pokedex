e03a40d5279d0b3f8fc5acc7d88d527f5925a9b8
Leo Kling <zugecraftet@gmail.com>
2021-11-14 16:53:45 +0100

    ✨feature: new-pokemon-pages

--- components/PokeCard.js
+++ components/PokeCard.js
@@ -6,13 +6,15 @@ import styles from "../styles/PokeCard.module.css";
 
 function PokeCard({ id, name }) {
   return (
-    <Link href={`/pokemon/${id}`}>
+    <Link href={`/pokemon/${id}`} passHref>
       <div className={styles.card}>
         <Image
           src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`}
           width="128px"
           height="128px"
+          alt={`An image of ${name.charAt(0).toUpperCase() + name.slice(1)}`}
         />
+        <p>#{id}</p>
         <p>{name.charAt(0).toUpperCase() + name.slice(1)}</p>
       </div>
     </Link>
--- pages/pokemon/[id].js
+++ pages/pokemon/[id].js
@@ -1,7 +1,38 @@
 import { useRouter } from "next/dist/client/router";
 
-const Pokemon = ({ data }) => {
-  return <pre>{JSON.stringify(data, null, 2)}</pre>;
+const Pokemon = ({ data, id, gender, characteristic }) => {
+  return (
+    <div>
+      <h1>
+        {data.name.charAt(0).toUpperCase() + data.name.slice(1)} #{id}
+      </h1>
+
+      <div>
+        <h3>Height</h3>
+        <p>{data.height}</p>
+      </div>
+      <div>
+        <h3>Weight</h3>
+        <p>{data.weight}</p>
+      </div>
+      <div>
+        <h3>Gender</h3>
+        <p>
+          {gender.map((gender, i) => {
+            return <span key={i}>{gender} </span>;
+          })}
+        </p>
+      </div>
+      <div>
+        <h3>Characteristic</h3>
+        <p>
+          {characteristic
+            ? characteristic.descriptions.at(-1).description
+            : "N/A"}
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export const getStaticPaths = async () => {
@@ -21,15 +52,32 @@ export const getStaticPaths = async () => {
 };
 
 export const getStaticProps = async ({ params }) => {
-  const response = await fetch(
-    `https://pokeapi.co/api/v2/pokemon/${params.id}/`
-  );
+  const id = params.id;
 
+  let response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`);
   const data = await response.json();
+
+  response = await fetch(`https://pokeapi.co/api/v2/characteristic/${id}/`);
+  const characteristic = response.status == 200 ? await response.json() : null;
+
+  response = await fetch(`https://pokeapi.co/api/v2/gender/1/`);
+  const female = JSON.stringify(await response.json());
+  response = await fetch(`https://pokeapi.co/api/v2/gender/2/`);
+  const male = JSON.stringify(await response.json());
+  response = await fetch(`https://pokeapi.co/api/v2/gender/3/`);
+  const genderless = JSON.stringify(await response.json());
+
+  const gender = [];
+  if (male.includes(data.name)) gender.push("Male");
+  if (female.includes(data.name)) gender.push("Female");
+  if (genderless.includes(data.name)) gender.push("Genderless");
 
   return {
     props: {
       data: data,
+      id: id,
+      characteristic: characteristic,
+      gender: gender,
     },
   };
 };
